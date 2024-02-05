a1f34b7d12d657efdec1a88610b045d9e693eb36
Sarah <75185314+sarisp3260@users.noreply.github.com>
2024-02-04 19:30:28 -0500

    home message still in progress :hammer:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom"
 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="text-white text-center mx-10 p-4 space-y-5">
+      <h1>Home</h1>
+
+      <p><strong>Climate Change</strong> is a web application that allows you to calculate the temperature of an object. The temperature of an object is measured in units called degrees. The temperature of an object can be measured using a thermometer. The temperature of an object can be measured in degrees Celsius (°C) or degrees Fahrenheit (°F).</p>
+
+      <p className="py-5">We aren't finish this section go to </p> 
+      <Link className="underline underline-offset-8 decoration-cyan-400" to="/temperature">Temperature page</Link>
+    </div>
   )
 }
 
